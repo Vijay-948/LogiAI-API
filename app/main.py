a66cb519cb70dc5bfeede2dd0541448b8cac7d70
vijay-948 <kvijayreddy948@gmail.com>
2025-12-11 23:03:47 +0530

    post-api

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/analyze-log")
+async def analyze_log(file: UploadFile = File(...)):
+    content = await file.read()
+    log_text = content.decode("utf-8")
+    return {"status": "ok", "length": len(log_text)}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
